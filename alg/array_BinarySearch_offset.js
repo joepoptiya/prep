994b80b1a849c39140e184f0c474b74c8f2c801f
Joe Poptiya <joepop@microsoft.com>
2020-03-06 01:48:46 -0800

    Pivot

--- alg/array_BinarySearch_offset.js
+++ alg/array_BinarySearch_offset.js
@@ -6,41 +6,25 @@ function PrintArray(arr) {
     console.log(s);
 }
 
-function GetPivot(arr, left, right) {
-    let mid = Math.trunc((right - left) / 2);
+function GetPivot(arr) {
     let left = 0;
     let right = arr.length - 1;
 
     while (left < right) {
-        let mid = left + (right - left) / 2;
+        let mid = Math.trunc(left + (right - left) / 2);
 
         if (arr[mid] > arr[right])
-            
+            left = mid+1;
+        else 
+            right = mid;
     }
 
-    /*
-    console.log(left, mid, right);
-    console.log(arr[left], arr[mid], arr[right]);
-    // 6, 7, 8, 1, 2, 3, 4, 5
-    // 6, 7, 8, 9, 10, 1, 2, 3, 4, 5
-    if (arr[left] > arr[mid]) {
-        console.log("Move left");
-        console.log();
-        return GetPivot(arr, left, mid-1);
-    }
-    else if (arr[mid] > arr[right]) {
-        console.log("Move left");
-        console.log();
-        return GetPivot(arr, mid+1, right);
-    }
-    else
-        return mid;
-    */
+    return left;
 }
 
 
 function SearchIndex(arr, k) {
-    let pivot = GetPivot(arr, 0, arr.length-1);
+    let pivot = GetPivot(arr);
     return pivot;
 }
 
